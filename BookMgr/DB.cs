1e16e04cde6456d2dba4cc159a681da06d920154
Marlin-1013 <84124723+Marlin-1013@users.noreply.github.com>
2021-12-10 23:51:55 +0900

    DB추가

---
+++ BookMgr/DB.cs
@@ -0,0 +1,58 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace BookMgr
+{
+    class DB
+    {
+        MySqlConnection con = new MySqlConnection("Server=localhost;Database=rentalbook;Uid=root;Pwd=123456");
+        public void connect()
+        {
+            if (con.State.ToString().Equals("Closed"))
+            {
+                con.Open();
+            }
+        }
+
+        public void close()
+        {
+            if (con != null)
+            {
+                con.Close();
+            }
+        }
+
+        public void insertQuery(String sql)
+        {
+
+            MySqlCommand com = new MySqlCommand(sql, con);
+            try
+            {
+                if (com.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("성공");
+                }
+                else
+                {
+                    MessageBox.Show("실패");
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                MessageBox.Show("DB오류가 발생했습니다.");
+            }
+        }
+
+        public DataTable showDBTable(String sql)
+        {
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sql, con);
+            MySqlCommandBuilder builder = new MySqlCommandBuilder(adapter);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            return dt;
+        }
+    }
+}
