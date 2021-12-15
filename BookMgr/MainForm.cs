abf3efa645242a56a7732f4995bf639e6218b928
석찬 고 <99gookan@naver.com>
2021-12-15 19:48:15 +0900

    오류수정

--- BookMgr/MainForm.cs
+++ BookMgr/MainForm.cs
@@ -164,32 +164,12 @@ namespace BookMgr
                 MessageBox.Show("비밀번호를 잘못 입력했습니다.", "비밀번호 오류", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
-        private void SameCheckbtn_Click(object sender, EventArgs e) // 유저-회원정보수정-아이디중복확인 버튼
-        {
-            String UpID = UpdateIDtxt.Text;
-
-            if (UpdateIDtxt.Text == "1234") //아이디중복화인
-            {
-                MessageBox.Show("이미 등록된 아이디입니다.", "아이디 중복", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                UpdateIDtxt.Text = "";
-                ;
-            }
-            else if (UpdateIDtxt.Text == "") //빈칸
-            {
-                MessageBox.Show("아이디를 입력해주세요.", "아이디 오류", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                UpdateIDtxt.Text = "";
-                ;
-            }
-            else
-            {
-                MessageBox.Show("사용가능한 아이디입니다.", "아이디 사용가능", MessageBoxButtons.OK, MessageBoxIcon.None);
-            }
-        }
+      
 
         private void Updatebtn_Click(object sender, EventArgs e) // 유저-회원정보수정-수정완료 버튼
         {
 
-            if (UpdatePW1txt.Text == "" || UpdatePW2txt.Text == "" || Emailtxt.Text == "" || Nametxt.Text == "" || Teltxt1.Text == "") // 입력오류 
+            if (UpdatePW1txt.Text == "" || UpdatePW2txt.Text == "" || Emailtxt.Text == "" || Nametxt.Text == "" || Teltxt.Text == "") // 입력오류 
             {
                 MessageBox.Show("필수 항목을 입력해주세요.", "입력 오류", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
@@ -202,7 +182,7 @@ namespace BookMgr
                 DialogResult result = MessageBox.Show("회원정보를 수정하시겠습니까?", "회원정보수정", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                 if (result == DialogResult.Yes)
                 {
-                    string sql = "update user set(`PW`, `Email`, `Tel`, `Name`, `Adress`) = ('" + UpdatePW1txt.Text + "', '" + Emailtxt.Text + "@" + EmailCbx.Text + "', '" + Teltxt1.Text + "', '" + Nametxt.Text + "', '" + Addresstxt.Text + "');";
+                    string sql = "update user set(`PW`, `Email`, `Tel`, `Name`, `Adress`) = ('" + UpdatePW1txt.Text + "', '" + Emailtxt.Text + "@" + EmailCbx.Text + "', '" + Teltxt.Text + "', '" + Nametxt.Text + "', '" + Addresstxt.Text + "');";
                     db.connect();
                     db.insertQuery(sql);
                     db.close();
@@ -379,7 +359,7 @@ namespace BookMgr
         private void SrchBookbtn_Click(object sender, EventArgs e) // 도서-도서검색-도서검색 버튼
         {
 
-            string title = SrchTitletxt.Text, publisher = SrchPublishertxt.Text, author = Srchauthortxt.Text, date = SrchDatetxt1.Text, ISBN = SrchISBNtxt.Text;
+            string title = SrchTitletxt.Text, publisher = SrchPublishertxt.Text, author = Srchauthortxt.Text, date = SrchDatetxt.Text, ISBN = SrchISBNtxt.Text;
             string query = "SELECT * FROM Books WHERE ";
             var list = new List<string>();
             if (title.Length > 0)
@@ -542,7 +522,7 @@ namespace BookMgr
                 SrchTitletxt.Text = FindGridView.Rows[e.RowIndex].Cells[1].Value.ToString();
                 Srchauthortxt.Text = FindGridView.Rows[e.RowIndex].Cells[2].Value.ToString();
                 SrchPublishertxt.Text = FindGridView.Rows[e.RowIndex].Cells[3].Value.ToString();
-                SrchDatetxt1.Text = FindGridView.Rows[e.RowIndex].Cells[4].Value.ToString();
+                SrchDatetxt.Text = FindGridView.Rows[e.RowIndex].Cells[4].Value.ToString();
                 SrchISBNtxt.Text = FindGridView.Rows[e.RowIndex].Cells[5].Value.ToString();
             }
             catch
